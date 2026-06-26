873a72e5db5fffceac66c088d594283ed0db8d94
Michał Górny <mgorny@quansight.com>
2026-06-26 21:00:49 +0200

    Modify the `pypi.io` migrator to use `files.pythonhosted.org`
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- conda_forge_tick/migrators/pypi_org.py
+++ conda_forge_tick/migrators/pypi_org.py
@@ -5,16 +5,18 @@ from conda_forge_tick.migrators.core import MiniMigrator, skip_migrator_due_to_s
 from conda_forge_tick.migrators.libboost import _replacer
 
 # detect "url: https://pypi.io/packages/..."
-raw_pat_pypi_io = r"^(?P<before>[\s\-]*url: )(?:https?://pypi\.io/)(?P<after>.*)"
-pat_pypi_io = re.compile(raw_pat_pypi_io)
+raw_pat_legacy_pypi = (
+    r"^(?P<before>[\s\-]*url: )(?:https?://pypi\.(?:io|org)/packages/)(?P<after>.*)"
+)
+pat_legacy_pypi = re.compile(raw_pat_legacy_pypi)
 
 
 class PyPIOrgMigrator(MiniMigrator):
     def filter(self, attrs, not_bad_str_start=""):
         lines = attrs["raw_meta_yaml"].splitlines()
-        has_pypi_io = any(pat_pypi_io.search(line) for line in lines)
+        has_legacy_pypi = any(pat_legacy_pypi.search(line) for line in lines)
         # filter() returns True if we _don't_ want to migrate
-        return (not has_pypi_io) or skip_migrator_due_to_schema(
+        return (not has_legacy_pypi) or skip_migrator_due_to_schema(
             attrs, self.allowed_schema_versions
         )
 
@@ -25,7 +27,9 @@ class PyPIOrgMigrator(MiniMigrator):
                 lines = fp.readlines()
 
             lines = _replacer(
-                lines, raw_pat_pypi_io, r"\g<before>https://pypi.org/\g<after>"
+                lines,
+                raw_pat_legacy_pypi,
+                r"\g<before>https://files.pythonhosted.org/packages/\g<after>",
             )
 
             with open(fname, "w") as fp:
