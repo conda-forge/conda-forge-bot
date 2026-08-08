70afedc7b908ebaf047ac0beba6924bec02c7447
H. Vetinari <h.vetinari@gmx.com>
2026-08-09 07:35:07 +1100

    ignore some problematic packages in riscv migrator

--- conda_forge_tick/migrators/arch.py
+++ conda_forge_tick/migrators/arch.py
@@ -483,6 +483,14 @@ class LinuxRISCV64(_CrossCompileRebuild):
     bump_number = 1
     pkg_list_filename = "linux_riscv64.txt"
     arches = {"linux_riscv64": "linux_64"}
+    ignored_packages = {
+        # already built compiler packages that get caught in a cycle
+        "_openmp_mutex",
+        "ctng-compiler-activation",
+        "ctng-compilers",
+        "gfortran_impl_osx-64",
+        "gfortran_osx-64",
+    }
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("name", "support linux riscv64 platform")
