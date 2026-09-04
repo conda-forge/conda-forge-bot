28589a2ab069da4ef7cfdc9bf32622d343b6c36b
Michał Górny <mgorny@quansight.com>
2026-09-04 14:49:10 +0200

    Update the integration
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- conda_forge_tick/make_migrators.py
+++ conda_forge_tick/make_migrators.py
@@ -106,6 +106,7 @@ DEFAULT_MINI_MIGRATORS = [
     DuplicateLinesCleanup,
     PipMigrator,
     LicenseMigrator,
+    CrossToNativeMigrator,
     CondaForgeYAMLCleanup,
     ExtraJinja2KeysCleanup,
     NoCondaInspectMigrator,
@@ -1041,27 +1042,6 @@ def add_cdt_migrator(
         migrators[-1].pr_limit = pr_limit
 
 
-def add_cross_to_native_migrator(
-    migrators: MutableSequence[Migrator],
-    gx: nx.DiGraph,
-):
-    with fold_log_lines("making cross-to-native migrator"):
-        migrators.append(
-            CrossToNativeMigrator(
-                total_graph=gx,
-                pr_limit=PR_LIMIT,
-                piggy_back_migrations=_make_mini_migrators_with_defaults(
-                    extra_mini_migrators=[YAMLRoundTrip()],
-                ),
-            )
-        )
-        pr_limit, _, _ = _compute_migrator_pr_limit(
-            migrators[-1],
-            PR_LIMIT,
-        )
-        migrators[-1].pr_limit = pr_limit
-
-
 def _make_version_migrator(
     gx: nx.DiGraph,
     dry_run: bool = False,
@@ -1121,8 +1101,6 @@ def initialize_migrators(
     add_static_lib_migrator(migrators, gx, job=job, n_jobs=n_jobs)
 
     add_cdt_migrator(migrators, gx, job=job, n_jobs=n_jobs)
-
-    add_cross_to_native_migrator(migrators, gx)
 
     pinning_migrators: list[Migrator] = []
     migration_factory(
