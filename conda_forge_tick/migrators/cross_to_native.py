8520c771eac88412d14b3e25c7700e4706b8c2e8
Isuru Fernando <isuruf@gmail.com>
2026-09-04 08:53:10 -0700

    Revert "Extend `CrossToNativeMigrator` to `osx-arm64` (#6647)"
    This reverts commit ea41e4944200cb2a9eb5fcb3b0097375345293c0.

--- conda_forge_tick/migrators/cross_to_native.py
+++ conda_forge_tick/migrators/cross_to_native.py
@@ -15,20 +15,18 @@ from conda_forge_tick.utils import (
 class CrossToNativeMigrator(MiniMigrator):
     allowed_schema_versions = {0, 1}
 
-    # migrated platforms -> their default CI providers
-    _platform_providers = {
-        "linux_aarch64": "github_actions",
-        "osx_arm64": "azure",
-    }
+    # platforms to migrate
+    _platforms = {"linux_aarch64"}
+    # build platforms where default = github_actions
+    _gha_platforms = {"linux_64"}
 
     def _migrate_platform(self, platform: str, cfyaml: CondaForgeYamlContents) -> bool:
         build_platform = cfyaml.get("build_platform", {}).get(platform)
         if build_platform is None:
             return False
         provider = cfyaml.get("provider", {}).get(build_platform, "default")
-        return platform in self._platform_providers and provider in (
-            self._platform_providers[platform],
-            "default",
+        return provider == "github_actions" or (
+            provider == "default" and build_platform in self._gha_platforms
         )
 
     def filter(self, attrs: "AttrsTypedDict", not_bad_str_start: str = "") -> bool:
@@ -38,7 +36,7 @@ class CrossToNativeMigrator(MiniMigrator):
 
         # TODO: check if there are any relevant cross-targets
         cfyaml = attrs.get("conda-forge.yml", {})
-        for platform in self._platform_providers:
+        for platform in self._platforms:
             if self._migrate_platform(platform, cfyaml):
                 return False
         return True
@@ -48,7 +46,7 @@ class CrossToNativeMigrator(MiniMigrator):
         with open(cfyaml_path) as fp:
             cfyaml = yaml_safe_load(fp)
 
-        for platform in self._platform_providers:
+        for platform in self._platforms:
             if not self._migrate_platform(platform, cfyaml):
                 continue
 
