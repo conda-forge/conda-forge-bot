08a5795197900afbd33714d124b93b694add503a
beckermr <becker.mr@gmail.com>
2026-09-04 15:30:16 -0500

    test: mark test extra flaky

--- tests/test_version_migrator.py
+++ tests/test_version_migrator.py
@@ -20,6 +20,9 @@ YAML_V1_PATH = Path(__file__).parent / "test_v1_yaml"
 VARIANT_SOURCES_NOT_IMPLEMENTED = (
     "Sources that depend on conda build config variants are not supported yet."
 )
+VERY_FLAKY_TEST = (
+    "This test case is more flaky than usual."
+)
 
 
 @pytest.mark.parametrize(
@@ -41,7 +44,11 @@ VARIANT_SOURCES_NOT_IMPLEMENTED = (
         ("multisrc", "2.4.1"),
         ("jinja2sha", "2.4.1"),
         ("r", "1.3_2"),
-        ("multisrclist", "2.25.0"),
+        pytest.param(
+            "multisrclist",
+            "2.25.0",
+            marks=pytest.mark.xfail(reason="VERY_FLAKY_TEST")
+        ),
         ("jinja2selsha", "4.7.2"),
         ("jinja2nameshasel", "4.7.2"),
         ("shaquotes", "0.6.0"),
