eaea96461481845dcca6a0a4943fc9b7da8ec7e4
Ludovic Henry <git@ludovic.dev>
2026-09-14 18:22:08 +0200

    Refactor migrator code in common

--- conda_forge_tick/migrators/arch.py
+++ conda_forge_tick/migrators/arch.py
@@ -1,5 +1,6 @@
 import copy
 import os
+from abc import abstractmethod
 from collections.abc import Collection, Sequence
 from textwrap import dedent
 from typing import Any, Literal
@@ -190,178 +191,23 @@ class _ArchesConfiguredMixin:
         return _arches_are_configured(attrs, self.arches)
 
 
-class ArchRebuild(_ArchesConfiguredMixin, GraphMigrator):
-    """A Migrator that adds aarch64 builds to feedstocks."""
-
-    allowed_schema_versions = {0, 1}
-    migrator_version = 1
-    rerender = True
-    # We purposefully don't want to bump build number for this migrator
-    bump_number = 0
-    ignored_packages = {
-        "make",
-        "perl",
-        "toolchain",
-        "posix",
-        "patchelf",  # weird issue
-    }
-    arches = {
-        "linux_aarch64": "default",
-    }
-
-    def __init__(
-        self,
-        graph: nx.DiGraph | None = None,
-        # do not change the name, but this doesn't actually add ppc anymore
-        name: str = "aarch64 and ppc64le addition",
-        pr_limit: int = 0,
-        piggy_back_migrations: Sequence[MiniMigrator] | None = None,
-        target_packages: Collection[str] | None = None,
-        effective_graph: nx.DiGraph | None = None,
-        total_graph: nx.DiGraph | None = None,
-        top_level: set["PackageName"] | None = None,
-    ):
-        if total_graph is not None:
-            if target_packages is None:
-                # We are constraining the scope of this migrator
-                target_packages = load_target_packages("arch_rebuild.txt")
-
-            outputs_lut = get_outputs_lut(total_graph, graph, effective_graph)
-
-            # rebuild the graph to only use edges from the arm and power requirements
-            graph2 = nx.create_empty_copy(total_graph)
-            for node, attrs in total_graph.nodes(data="payload"):
-                for plat_arch in self.arches:
-                    deps = set().union(
-                        *attrs.get(
-                            f"{plat_arch}_requirements",
-                            attrs.get("requirements", {}),
-                        ).values()
-                    )
-                    for dep in get_deps_from_outputs_lut(deps, outputs_lut):  # type: ignore[arg-type]
-                        graph2.add_edge(dep, node)
-                pass
-
-            total_graph = graph2
-            target_packages = set(target_packages)
-            if target_packages:
-                target_packages.add("python")  # hack that is ~harmless?
-                total_graph = cut_graph_to_target_packages(total_graph, target_packages)
-
-            # filter out stub packages and ignored packages
-            _filter_stubby_and_ignored_nodes(
-                total_graph, outputs_lut, self.ignored_packages
-            )
-
-        if not hasattr(self, "_init_args"):
-            self._init_args = []
-
-        if not hasattr(self, "_init_kwargs"):
-            self._init_kwargs = {
-                "graph": graph,
-                "name": name,
-                "pr_limit": pr_limit,
-                "piggy_back_migrations": piggy_back_migrations,
-                "target_packages": target_packages,
-                "effective_graph": effective_graph,
-                "total_graph": total_graph,
-            }
-
-        self.target_packages = target_packages
-
-        super().__init__(
-            graph=graph,
-            pr_limit=pr_limit,
-            check_solvable=False,
-            piggy_back_migrations=piggy_back_migrations,
-            effective_graph=effective_graph,
-            total_graph=total_graph,
-            name=name,
-            top_level=top_level,
-        )
-        assert not self.check_solvable, "We don't want to check solvability for aarch!"
-
-    def migrate(
-        self, recipe_dir: str, attrs: "AttrsTypedDict", **kwargs: Any
-    ) -> MigrationUidTypedDict | Literal[False]:
-        with pushd(recipe_dir + "/.."):
-            recipe_file = next(
-                filter(os.path.exists, ("recipe/recipe.yaml", "recipe/meta.yaml"))
-            )
-            self.set_build_number(recipe_file)
-
-            with open("conda-forge.yml") as f:
-                y = yaml_safe_load(f)
-
-            y_orig = copy.deepcopy(y)
-
-            if "provider" not in y:
-                y["provider"] = {}
-            for k, v in self.arches.items():
-                if k not in y["provider"]:
-                    y["provider"][k] = v
-
-            with open("conda-forge.yml", "w") as f:
-                yaml_safe_dump(y, f)
-
-        muid = super().migrate(recipe_dir, attrs, **kwargs)
-        if muid is False:
-            return False
-        if y_orig == y:
-            muid["already_done"] = True
-
-        return muid
-
-    def pr_title(self, feedstock_ctx: FeedstockContext) -> str:
-        title = "Linux aarch64 Migrator"
-        branch = feedstock_ctx.attrs.get("branch", "main")
-        if branch not in ["main", "master"]:
-            return f"[{branch}] " + title
-        else:
-            return title
-
-    def pr_body(
-        self, feedstock_ctx: ClonedFeedstockContext, add_label_text: bool = False
-    ) -> str:
-        body = super().pr_body(feedstock_ctx)
-        body = body.format(
-            dedent(
-                """\
-        This feedstock is being rebuilt as part of the aarch64 migration.
-
-        **Feel free to merge the PR if CI is all green, but please don't close it
-        without reaching out the the ARM migrators first at <code>@</code>conda-forge/arm-arch.**
-        """,
-            ),
-        )
-        return body
-
-    def remote_branch(self, feedstock_ctx: FeedstockContext) -> str:
-        return super().remote_branch(feedstock_ctx) + "_arch"
-
-
-class _CrossCompileRebuild(_ArchesConfiguredMixin, GraphMigrator):
-    """A Migrator that adds arch platform builds to feedstocks."""
-
-    rerender = True
-    # We purposefully don't want to bump build number for this migrator
-    bump_number = 0
+class _Rebuild(GraphMigrator):
+    """Common code for native and cross-compile migrators."""
 
     ignored_packages: set[str] = set()
     excluded_dependencies: set[str] = set()
 
-    @property
-    def additional_keys(self):
-        return {
-            "build_platform": self.build_platform,  # type: ignore[attr-defined]
-            "test": "native_and_emulated",
-        }
+    pkg_list_filename: str = ""
+
+    title: str = ""
+    migration_name: str = ""
+    migration_team: str = ""
 
     def __init__(
         self,
         graph: nx.DiGraph | None = None,
-        pr_limit: int = 0,
         name: str = "",
+        pr_limit: int = 0,
         piggy_back_migrations: Sequence[MiniMigrator] | None = None,
         target_packages: Collection[str] | None = None,
         effective_graph: nx.DiGraph | None = None,
@@ -371,36 +217,14 @@ class _CrossCompileRebuild(_ArchesConfiguredMixin, GraphMigrator):
         if total_graph is not None:
             if target_packages is None:
                 # We are constraining the scope of this migrator
-                target_packages = load_target_packages(self.pkg_list_filename)  # type: ignore[attr-defined]
+                target_packages = load_target_packages(self.pkg_list_filename)
+
             outputs_lut = get_outputs_lut(total_graph, graph, effective_graph)
 
             # rebuild the graph to only use edges from the arch requirements
             graph2 = nx.create_empty_copy(total_graph)
             for node, attrs in total_graph.nodes(data="payload"):
-                for plat_arch, build_plat_arch in self.build_platform.items():  # type: ignore[attr-defined]
-                    reqs = attrs.get(
-                        f"{plat_arch}_requirements",
-                        attrs.get(
-                            f"{build_plat_arch}_requirements",
-                            attrs.get("requirements", {}),
-                        ),
-                    )
-                    host_deps = set(as_iterable(reqs.get("host", set())))
-                    run_deps = set(as_iterable(reqs.get("run", set())))
-                    deps = host_deps.union(run_deps)
-
-                    # We are including the compiler stubs here so that
-                    # excluded_dependencies work correctly.
-                    # Edges to these compiler stubs are removed afterwards
-                    build_deps = set(as_iterable(reqs.get("build", set())))
-                    for build_dep in build_deps:
-                        if build_dep.endswith("_stub"):
-                            deps.add(build_dep)
-                    for dep in get_deps_from_outputs_lut(
-                        deps,
-                        outputs_lut,  # type: ignore[arg-type]
-                    ):
-                        graph2.add_edge(dep, node)
+                self._add_edge_for_node(graph2, node, attrs, outputs_lut)
 
             total_graph = graph2
 
@@ -449,6 +273,14 @@ class _CrossCompileRebuild(_ArchesConfiguredMixin, GraphMigrator):
         )
         assert not self.check_solvable, "We don't want to check solvability!"
 
+    @abstractmethod
+    def _add_edge_for_node(self, graph2, node, attrs, outputs_lut):
+        pass
+
+    @abstractmethod
+    def _migrate_conda_forge_yml(self, yaml, attrs: "AttrsTypedDict"):
+        pass
+
     def migrate(
         self, recipe_dir: str, attrs: "AttrsTypedDict", **kwargs: Any
     ) -> MigrationUidTypedDict | Literal[False]:
@@ -463,30 +295,153 @@ class _CrossCompileRebuild(_ArchesConfiguredMixin, GraphMigrator):
 
             y_orig = copy.deepcopy(y)
 
-            # we should do this recursively but the cf yaml is usually
-            # one key deep so this is fine
-            for k, v in self.additional_keys.items():
-                if isinstance(v, dict):
-                    if k not in y:
-                        y[k] = {}
-                    for _k, _v in v.items():
-                        y[k][_k] = _v
-                else:
-                    y[k] = v
+            self._migrate_conda_forge_yml(y, attrs)
 
             with open("conda-forge.yml", "w") as f:
                 yaml_safe_dump(y, f)
 
         muid = super().migrate(recipe_dir, attrs, **kwargs)
         if muid is False:
-            return muid
+            return False
         if y_orig == y:
             muid["already_done"] = True
 
         return muid
 
+    def pr_title(self, feedstock_ctx: FeedstockContext) -> str:
+        branch = feedstock_ctx.attrs.get("branch", "main")
+        if branch not in ["main", "master"]:
+            return f"[{branch}] " + self.title
+        else:
+            return self.title
 
-class OSXArm(_CrossCompileRebuild):
+    def pr_body(
+        self, feedstock_ctx: ClonedFeedstockContext, add_label_text: bool = False
+    ) -> str:
+        body = super().pr_body(feedstock_ctx)
+        body = body.format(
+            dedent(
+                f"""\
+        This feedstock is being rebuilt as part of the {self.migration_name} migration.
+
+        **Feel free to merge the PR if CI is all green, but please don't close it
+        without reaching out first to {self.migration_team}.**
+        """,
+            ),
+        )
+        return body
+
+
+class _NativeCompileMixin(_ArchesConfiguredMixin):
+    """A Migrator that adds native builds to feedstocks."""
+
+    def _add_edge_for_node(self, graph2, node, attrs, outputs_lut):
+        for plat_arch in self.arches:
+            deps = set().union(
+                *attrs.get(
+                    f"{plat_arch}_requirements",
+                    attrs.get("requirements", {}),
+                ).values()
+            )
+            for dep in get_deps_from_outputs_lut(deps, outputs_lut):  # type: ignore[arg-type]
+                graph2.add_edge(dep, node)
+
+    def _migrate_conda_forge_yml(self, yaml, attrs: "AttrsTypedDict"):
+        if "provider" not in yaml:
+            yaml["provider"] = {}
+        for k, v in self.arches.items():
+            if k not in yaml["provider"]:
+                yaml["provider"][k] = v
+
+
+class ArchRebuild(_NativeCompileMixin, _Rebuild):
+    """A Migrator that adds aarch64 builds to feedstocks."""
+
+    allowed_schema_versions = {0, 1}
+    migrator_version = 1
+    rerender = True
+    # We purposefully don't want to bump build number for this migrator
+    bump_number = 0
+    pkg_list_filename = "arch_rebuild.txt"
+    arches = {
+        "linux_aarch64": "default",
+    }
+    ignored_packages = {
+        "make",
+        "perl",
+        "toolchain",
+        "posix",
+        "patchelf",  # weird issue
+    }
+
+    title = "Linux aarch64 Migrator"
+    migration_name = "aarch64"
+    migration_team = "<code>@</code>conda-forge/arm-arch"
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("name", "aarch64 and ppc64le addition")
+        super().__init__(*args, **kwargs)
+
+    def remote_branch(self, feedstock_ctx: FeedstockContext) -> str:
+        return super().remote_branch(feedstock_ctx) + "_arch"
+
+
+class _CrossCompileMixin(_ArchesConfiguredMixin):
+    """A Migrator that adds arch platform builds to feedstocks."""
+
+    rerender = True
+    # We purposefully don't want to bump build number for this migrator
+    bump_number = 0
+
+    build_platform: dict = {}
+
+    @property
+    def additional_keys(self):
+        return {
+            "build_platform": self.build_platform,
+            "test": "native_and_emulated",
+        }
+
+    def _add_edge_for_node(self, graph2, node, attrs, outputs_lut):
+        for plat_arch, build_plat_arch in self.build_platform.items():
+            reqs = attrs.get(
+                f"{plat_arch}_requirements",
+                attrs.get(
+                    f"{build_plat_arch}_requirements",
+                    attrs.get("requirements", {}),
+                ),
+            )
+            host_deps = set(as_iterable(reqs.get("host", set())))
+            run_deps = set(as_iterable(reqs.get("run", set())))
+            deps = host_deps.union(run_deps)
+
+            # We are including the compiler stubs here so that
+            # excluded_dependencies work correctly.
+            # Edges to these compiler stubs are removed afterwards
+            build_deps = set(as_iterable(reqs.get("build", set())))
+            for build_dep in build_deps:
+                if build_dep.endswith("_stub"):
+                    deps.add(build_dep)
+            for dep in get_deps_from_outputs_lut(
+                deps,
+                outputs_lut,  # type: ignore[arg-type]
+            ):
+                graph2.add_edge(dep, node)
+
+    def _migrate_conda_forge_yml(self, yaml, attrs: "AttrsTypedDict"):
+        # we should do this recursively but the cf yaml is usually
+        # one key deep so this is fine
+        for k, v in self.additional_keys.items():
+            if isinstance(v, dict):
+                if k not in yaml:
+                    yaml[k] = {}
+                for _k, _v in v.items():
+                    yaml[k][_k] = _v
+            else:
+                yaml[k] = v
+
+
+class OSXArm(_CrossCompileMixin, _Rebuild):
     """A Migrator that adds osx-arm64 builds to feedstocks."""
 
     allowed_schema_versions = {0, 1}
@@ -495,39 +450,19 @@ class OSXArm(_CrossCompileRebuild):
     pkg_list_filename = "osx_arm64.txt"
     arches = {"osx_arm64": "osx_64"}
 
+    title = "ARM OSX Migrator"
+    migration_name = "ARM OSX"
+    migration_team = "<code>@</code>conda-forge/help-osx-arm64"
+
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("name", "arm osx addition")
         super().__init__(*args, **kwargs)
-
-    def pr_title(self, feedstock_ctx: FeedstockContext) -> str:
-        title = "ARM OSX Migrator"
-        branch = feedstock_ctx.attrs.get("branch", "main")
-        if branch not in ["main", "master"]:
-            return f"[{branch}] " + title
-        else:
-            return title
-
-    def pr_body(
-        self, feedstock_ctx: ClonedFeedstockContext, add_label_text: bool = True
-    ) -> str:
-        body = super().pr_body(feedstock_ctx)
-        body = body.format(
-            dedent(
-                """\
-        This feedstock is being rebuilt as part of the ARM OSX migration.
-
-        **Feel free to merge the PR if CI is all green, but please don't close it
-        without reaching out the the ARM OSX team first at <code>@</code>conda-forge/help-osx-arm64.**
-        """,  # noqa
-            ),
-        )
-        return body
 
     def remote_branch(self, feedstock_ctx: FeedstockContext) -> str:
         return super().remote_branch(feedstock_ctx) + "_arm_osx"
 
 
-class WinArm64(_CrossCompileRebuild):
+class WinArm64(_CrossCompileMixin, _Rebuild):
     """A Migrator that adds win-arm64 builds to feedstocks."""
 
     allowed_schema_versions = {0, 1}
@@ -541,39 +476,19 @@ class WinArm64(_CrossCompileRebuild):
         "gfortran_osx-64",
     }
 
+    title = "Support Windows ARM64 platform"
+    migration_name = "windows arm"
+    migration_team = "<code>@</code>conda-forge/help-win-arm64"
+
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("name", "support windows arm64 platform")
         super().__init__(*args, **kwargs)
-
-    def pr_title(self, feedstock_ctx: FeedstockContext) -> str:
-        title = "Support Windows ARM64 platform"
-        branch = feedstock_ctx.attrs.get("branch", "main")
-        if branch not in ["main", "master"]:
-            return f"[{branch}] " + title
-        else:
-            return title
-
-    def pr_body(
-        self, feedstock_ctx: ClonedFeedstockContext, add_label_text: bool = True
-    ) -> str:
-        body = super().pr_body(feedstock_ctx)
-        body = body.format(
-            dedent(
-                """\
-        This feedstock is being rebuilt as part of the windows arm migration.
-
-        **Feel free to merge the PR if CI is all green, but please don't close it
-        without reaching out the the ARM Windows team first at <code>@</code>conda-forge/help-win-arm64.**
-        """,
-            ),
-        )
-        return body
 
     def remote_branch(self, feedstock_ctx: FeedstockContext) -> str:
         return super().remote_branch(feedstock_ctx) + "_arm64_win"
 
 
-class LinuxRISCV64(_CrossCompileRebuild):
+class LinuxRISCV64(_CrossCompileMixin, _Rebuild):
     """A Migrator that adds linux-riscv64 builds to feedstocks."""
 
     allowed_schema_versions = {0, 1}
@@ -596,33 +511,13 @@ class LinuxRISCV64(_CrossCompileRebuild):
         "intel_repack",
     }
 
+    title = "Support linux-riscv64 platform"
+    migration_name = "linux riscv64"
+    migration_team = "<code>@</code>conda-forge/help-riscv64"
+
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("name", "support linux riscv64 platform")
         super().__init__(*args, **kwargs)
-
-    def pr_title(self, feedstock_ctx: FeedstockContext) -> str:
-        title = "Support linux-riscv64 platform"
-        branch = feedstock_ctx.attrs.get("branch", "main")
-        if branch not in ["main", "master"]:
-            return f"[{branch}] " + title
-        else:
-            return title
-
-    def pr_body(
-        self, feedstock_ctx: ClonedFeedstockContext, add_label_text: bool = True
-    ) -> str:
-        body = super().pr_body(feedstock_ctx)
-        body = body.format(
-            dedent(
-                """\
-                This feedstock is being rebuilt as part of the linux riscv64 migration.
-
-                **Feel free to merge the PR if CI is all green, but please don't close it
-                without reaching out the the linux-riscv64 team first at <code>@</code>conda-forge/help-riscv64.**
-                """,
-            ),
-        )
-        return body
 
     def remote_branch(self, feedstock_ctx: FeedstockContext) -> str:
         return super().remote_branch(feedstock_ctx) + "_riscv64"
