731b8748a714436c17d382370ab84916fab5e97f
Ludovic Henry <git@ludovic.dev>
2026-09-14 19:24:55 +0200

    Make LinuxRISCV64 migrator a native rebuild

--- conda_forge_tick/migrators/arch.py
+++ conda_forge_tick/migrators/arch.py
@@ -388,6 +388,45 @@ class ArchRebuild(_NativeCompileMixin, _Rebuild):
         super().__init__(*args, **kwargs)
 
 
+class LinuxRISCV64(_NativeCompileMixin, _Rebuild):
+    """A Migrator that adds riscv64 builds to feedstocks."""
+
+    allowed_schema_versions = {0, 1}
+    migrator_version = 1
+    rerender = True
+    # We purposefully don't want to bump build number for this migrator
+    bump_number = 1
+    pkg_list_filename = "linux_riscv64.txt"
+    arches = {
+        "linux_riscv64": "default",
+    }
+    ignored_packages = {
+        "make",
+        "perl",
+        "toolchain",
+        "posix",
+        "patchelf",  # weird issue
+        # already built compiler packages that get caught in a cycle
+        "_openmp_mutex",
+        "ctng-compiler-activation",
+        "ctng-compilers",
+        "gfortran_impl_osx-64",
+        "gfortran_osx-64",
+        # intel packages will not be built for riscv
+        "intel-compiler-repack",
+        "intel_repack",
+    }
+
+    migration_pr_title = "Support linux-riscv64 platform"
+    migration_name = "riscv64"
+    migration_team = "<code>@</code>conda-forge/help-riscv64"
+    migration_branch_suffix = "riscv64"
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("name", "support linux riscv64 platform")
+        super().__init__(*args, **kwargs)
+
+
 class _CrossCompileMixin(_ArchesConfiguredMixin):
     """A Migrator that adds arch platform builds to feedstocks."""
 
@@ -483,37 +522,4 @@ class WinArm64(_CrossCompileMixin, _Rebuild):
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("name", "support windows arm64 platform")
-        super().__init__(*args, **kwargs)
-
-
-class LinuxRISCV64(_CrossCompileMixin, _Rebuild):
-    """A Migrator that adds linux-riscv64 builds to feedstocks."""
-
-    allowed_schema_versions = {0, 1}
-    migrator_version = 1
-    build_platform = {"linux_riscv64": "linux_64"}
-    # We bump here as most feedstocks need a rerender and updates
-    # the compiler versions
-    bump_number = 1
-    pkg_list_filename = "linux_riscv64.txt"
-    arches = {"linux_riscv64": "linux_64"}
-    ignored_packages = {
-        # already built compiler packages that get caught in a cycle
-        "_openmp_mutex",
-        "ctng-compiler-activation",
-        "ctng-compilers",
-        "gfortran_impl_osx-64",
-        "gfortran_osx-64",
-        # intel packages will not be built for riscv
-        "intel-compiler-repack",
-        "intel_repack",
-    }
-
-    migration_pr_title = "Support linux-riscv64 platform"
-    migration_name = "linux riscv64"
-    migration_team = "<code>@</code>conda-forge/help-riscv64"
-    migration_branch_suffix = "riscv64"
-
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("name", "support linux riscv64 platform")
         super().__init__(*args, **kwargs)
