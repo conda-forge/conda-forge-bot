fcf09317966f2b5da92a85d316ac3d067de7a5c8
beckermr <becker.mr@gmail.com>
2026-09-16 09:04:44 -0500

    fix: correct defaults

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -57,7 +57,7 @@ class TestBotSettings:
         assert bot_settings.graph_repo_default_branch == "main"
         assert (
             bot_settings.versions_github_backend_repo
-            == "conda-forge/conda-forge-bot-data"
+            == "conda-forge/conda-forge-bot-data-versions"
         )
         assert bot_settings.versions_repo_default_branch == "main"
         assert bot_settings.github_runner_debug is False
