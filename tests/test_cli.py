f3719ba5e725ce36035814d3a774945a15fd6d9f
beckermr <becker.mr@gmail.com>
2026-09-16 06:18:18 -0500

    fix: no more pull_changes option

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -173,7 +173,6 @@ def test_cli_mock_deploy_to_github_git_only(
         git_only=git_only,
         dirs_to_ignore=[],
         dirs_to_deploy=[],
-        pull_changes=False,
     )
 
 
@@ -197,7 +196,7 @@ def test_cli_mock_deploy_to_github_dirs_to_ignore(
 
     assert result.exit_code == 0
     cmd_mock.assert_called_once_with(
-        dry_run=False, git_only=False, dirs_to_deploy=[], pull_changes=False, **kws
+        dry_run=False, git_only=False, dirs_to_deploy=[], **kws
     )
 
 
