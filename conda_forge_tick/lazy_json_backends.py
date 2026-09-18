81dbc0c8497c2cf5b39fee8c79e1bce893f7cf31
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-09-18 12:09:39 -0500

    prod: try more often (#6808)

--- conda_forge_tick/lazy_json_backends.py
+++ conda_forge_tick/lazy_json_backends.py
@@ -86,7 +86,7 @@ def get_github_backend_repo_for_hashmap(hashmap_name: str) -> str:
     )
 
 
-def make_lazy_json_retry_sequence(num_tries=50, base=4, factor=1, max_wait=600):
+def make_lazy_json_retry_sequence(num_tries=50, base=2, factor=1, max_wait=600):
     def _func():
         for i in range(num_tries):
             start = factor * (base ** (i - 1))
