3a58b41ee351726fac9740bee1fb1b6ffcd76e71
Michał Górny <mgorny@quansight.com>
2026-09-23 16:10:08 +0200

    Resolve typing errors
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- conda_forge_tick/run_minimigrators.py
+++ conda_forge_tick/run_minimigrators.py
@@ -1,7 +1,9 @@
+import typing
 from pathlib import Path
 
 from conda_forge_tick.feedstock_parser import populate_feedstock_attributes
 from conda_forge_tick.make_migrators import _make_mini_migrators_with_defaults
+from conda_forge_tick.migrators_types import AttrsTypedDict
 from conda_forge_tick.utils import (
     yaml_safe_load,
 )
@@ -30,13 +32,16 @@ def run_minimigrators(
     with open(recipe_dir / recipe_filename) as fp:
         recipe_yml = fp.read()
 
-    pmy = populate_feedstock_attributes(
-        "",
-        {},
-        conda_forge_yaml=cf_yml,
-        feedstock_dir=feedstock_dir,
-        use_container=False,
-        **{(recipe_filename.replace(".", "_")): recipe_yml},
+    pmy = typing.cast(
+        AttrsTypedDict,
+        populate_feedstock_attributes(
+            "",
+            {},
+            conda_forge_yaml=cf_yml,
+            feedstock_dir=feedstock_dir,
+            use_container=False,
+            **{(recipe_filename.replace(".", "_")): recipe_yml},  # type: ignore[arg-type]
+        ),
     )
 
     migrators = _make_mini_migrators_with_defaults()
@@ -45,4 +50,4 @@ def run_minimigrators(
         filtered = migrator.filter(pmy)
         if not filtered:
             print(f"Running {migrator.__class__.__name__} minimigrator ...")
-            migrator.migrate(recipe_dir, pmy)
+            migrator.migrate(str(recipe_dir), pmy)
